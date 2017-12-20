89ccb537d9592a73d0f62801c3bfee53a0da991f
Shubham Gawas <33461135+Shubhamgawas@users.noreply.github.com>
2017-12-20 21:56:28 +0530

    Updated The  compilation issue

--- PROJECTA.C
+++ PROJECTA.C
@@ -1,38 +1,37 @@
 #include<stdio.h>
+void sum(int x,int y)
+  {
+   int z;
+   z=x+y;
+   printf("Addition : %d\n",z);
+  
+  }
 
-void main()
+ void mult(int x,int y)
+  {
+   int z;
+   z=x*y;
+   printf("Multiplication : %d\n",z);
+
+   }
+
+   void div(int x,int y)
+   {
+    int z;
+    z=x/y;
+    printf("Division : %d\n",z);
+   
+    }
+ void main()
 {
   int a,b,c,d,e,f;
-  clrscr();
+ 
   printf("Enter the Two Values:");
   scanf("%d%d",&a,&b);
 
   sum(a,b);
   mult(a,b);
   div(a,b);
-  getch();
+
 }
-  sum(int x,int y)
-  {
-   int z;
-   z=x+y;
-   printf("Addition : %d\n",z);
-   return 0;
-  }
-
-  mult(int x,int y)
-  {
-   int z;
-   z=x*y;
-   printf("Multiplication : %d\n",z);
-   return 0;
-   }
-
-   div(int x,int y)
-   {
-    int z;
-    z=x/y;
-    printf("Division : %d\n",z);
-    return 0;
-    }
-
+  
